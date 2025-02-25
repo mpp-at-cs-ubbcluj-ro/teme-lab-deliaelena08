808ed23ee5fcc71853133edf901d100ffcfae5fa
Tapuc Delia Elena <deliatapuc@gmail.com>
2025-02-25 08:30:49 +0200

    Retry commit for my first lab

--- Laborator1/Laborator1.java
+++ Laborator1/Laborator1.java
@@ -1,6 +1,6 @@
 public class Laborator1 {
     public static void main(String[] args){
 
-        System.out.println("Modul pentru laborator 1");
+        System.out.print("Modul pentru laborator 1");
     }
 }
